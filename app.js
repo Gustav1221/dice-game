d25d9e7a68c26d1e64ce08a2af572d2839dd1065
bolor <bolormunh12@gmail.com>
2024-10-29 15:16:56 +0800

    shoonnii zurgiig uurchildug bolov

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ var scores = [0,0];
 //toglogchiin eeljindee tsugluulsan onoog hadgalah huvisagch
 var roundScore = 2;
 //shoonii ali talaaraa buusniig hadgalah huvisagch, 1-6gesen utgiig ene huvisagchind sanamsarguigeer uusgej ugnu
-var dice = Math.floor(Math.random() * 6) + 1;
+
 // <div class="player-score" id="score-0">43</div> js-eer domruu handaj utgiig n uurchlj bn. textcontent gedeg ugugdulruun handaj bn yrunhiiduu 49gesen ug
 //window.document.querySelector('#score-0').textContent = dice;
 //document.querySelector("#score-1").innerHTML="<em›Yes!<em>";| textContentnd html bichij bolku html bichey gevel innerhtml
-const clck = document.querySelector('.btn-roll');
-function clickevent(){
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+var diceDOM = document.querySelector('.dice');
+//negl udaa negl gazar bichij bga bolhoor anonymous function bichiv
+document.querySelector('.btn-roll').addEventListener('click', function (){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDOM.style.display='block';
+    diceDOM.src='dice-'+diceNumber+'.png';
 }
-document.querySelector('.btn-roll').textContent = 0;
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-
-document.querySelector('.dice').style.display='none';
-
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
-
-console.log(' shoo ' +dice);
+);
